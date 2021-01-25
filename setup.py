ab8ffc0b46def48d251d725586d87ff06c0d9f9b
omkarudawant <omkarudawant97@gmail.com>
2021-01-25 16:10:52 +0530

    updated setup.py with download url for 0.0.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Omkar Udawant',
     author_email='omkarudawant97@gmail.com',
     url='https://github.com/omkarudawant/CutoML',
-    download_url='https://github.com/omkarudawant/CutoML/archive/0.0.8.tar.gz',
+    download_url='https://github.com/omkarudawant/CutoML/archive/0.0.9.tar.gz',
     keywords=[
         'pipeline optimization',
         'hyperparameter optimization',
