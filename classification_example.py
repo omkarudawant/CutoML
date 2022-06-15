4efee46bb71bad4dc6cc952e58eb7a045b08e34d
omkarudawant <omkarudawant97@gmail.com>
2022-06-15 23:14:13 +0530

    updated examples

--- classification_example.py
+++ classification_example.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
         dataset.data, dataset.target, test_size=0.25, random_state=0
     )
 
-    ctc = CutoClassifier(k_folds=5, n_jobs=2, random_state=0)
+    ctc = CutoClassifier(k_folds=5, n_jobs=2, random_state=1)
     ctc.fit(X=X_train, y=y_train)
-    print(ctc.score(X=X_test, y=y_test))
+    print(f'F1 Score: {ctc.score(X=X_test, y=y_test)}')
     print(
         ctc.best_estimator.named_steps["classification_model"].best_estimator_)
--- regression_example.py
+++ regression_example.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         dataset.data, dataset.target, test_size=0.25, random_state=0
     )
 
-    ctr = CutoRegressor(k_folds=5, n_jobs=2, verbose=0, random_state=0)
+    ctr = CutoRegressor(k_folds=5, n_jobs=2, random_state=1)
     ctr.fit(X=X_train, y=y_train)
-    print(ctr.score(X=X_test, y=y_test))
+    print(f'R2 Score: {ctr.score(X=X_test, y=y_test)}')
     print(ctr.best_estimator.named_steps["regression_model"].best_estimator_)
